10245708deedd83745216bce16bd37bf916c3098
Gabriel Dias <gabrielhdias148@gmail.com>
2021-06-27 01:40:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+url = 'https://www.amazon.com.br/'
+search = 'placa de vídeo nvidia'
+
+option = Options()
+option.headless = False
+driver = webdriver.Firefox(options=option)
+
+driver.get(url)
+
+driver.find_element_by_id('twotabsearchtextbox').send_keys(search)
+driver.find_element_by_id('nav-search-submit-button').click()
+
+products_html = driver.find_elements_by_xpath("//div[@class='a-section a-spacing-medium']")
+products_list = []
+
+for i in range(len(products_html)):
+    html_content = products_html[i].get_attribute('innerHTML')
+    soup = BeautifulSoup(html_content, 'lxml')
+    
+    title = soup.find('span', class_='a-size-base-plus a-color-base a-text-normal')
+    image = soup.find('img', class_='s-image')
+    price = soup.find('span', class_='a-price-whole')
+    decimal = soup.find('span', class_='a-price-fraction')
+    currency = soup.find('span', class_='a-price-symbol')
+
+    products_list.append({
+        'title': title.text if title else '',
+        'image': image['src'] if image else '',
+        'price':  price.text + decimal.text if price else '',
+        'currency': currency.text if currency else ''
+    })
+
+driver.quit()
+
+with open('data.json', 'w') as data:
+    json.dump(products_list, data, indent=4)
