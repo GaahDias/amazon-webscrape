b56061febb2b03c2a54a28c22e49be39c7105195
Gabriel Dias <gabrielhdias148@gmail.com>
2021-07-07 18:12:02 -0300

    Refactor code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 import sys
 from util.products import get_products
+from util.search import perform_search
 
 if __name__ == "__main__":
-    products = get_products(sys.argv[1])
+    driver = perform_search(sys.argv[1])
+    products = get_products(driver, pages=3)
 
     with open('data.json', 'w') as data:
         json.dump(products, data, indent=4, ensure_ascii=False)
--- util/products.py
+++ util/products.py
@@ -1,5 +1,5 @@
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-from .search import perform_search
 
 def product_template(soup):
     # Get data in the html with BeautifulSoup
@@ -18,39 +18,43 @@ def product_template(soup):
     })
     
     return product
+
+def get_page_data(driver, url):
+    driver.get(url)
+    products_list = []
+    # Getting container with products data, and adding a new 'page' to products_list
+    products_html = driver.find_elements_by_xpath("//div[@class='a-section a-spacing-medium']")
+
+    # Loop through all products in page
+    for i in range(len(products_html)):
+        print(f'Getting {i} products')
+        html_content = products_html[i].get_attribute('innerHTML')
+        soup = BeautifulSoup(html_content, 'lxml')
         
-def get_products(search, **kwargs):
+        products_list.append(product_template(soup))
+        
+    return products_list
+
+        
+def get_products(driver, **kwargs):
     products_dict = {}
-    driver = perform_search(search)
     
     # Loop through pages
     for i in range(kwargs['pages'] if kwargs else 10):
-        products_list = []
         # Sleep so the page can load
         driver.implicitly_wait(1)
         
         try:
             # Get next page element
             next_page = driver.find_element_by_xpath("//li[@class='a-last']//a")
-        except:
+        except Exception:
             print("WARNING: Last page")
             next_page = False
         finally:
-            # Getting container with products data, and adding a new 'page' to products_list
-            products_html = driver.find_elements_by_xpath("//div[@class='a-section a-spacing-medium']")
-
-            # Loop through all products in page
-            for j in range(len(products_html)):
-                print(f"PAGE {i}: Getting {j} products")
-                html_content = products_html[j].get_attribute('innerHTML')
-                soup = BeautifulSoup(html_content, 'lxml')
-                
-                products_list.append(product_template(soup))
-            
-            products_dict[f'page{i+1}'] = products_list
-            # Check if there's a next page, if not, quits driver and returns the list
+            # Check if there's a next page, if not, quits driver and returns the dict
             if next_page:
-                next_page.click()
+                driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't') 
+                products_dict[f'page{i+1}'] = get_page_data(driver, next_page.get_attribute("href"))
             else:
                 break
             
